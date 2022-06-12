48320eb3d1b37ed4f00c961d2a655115b6df6cac
ELi Ben-Solomon <jq@Jareds-MacBook-Pro-4.local>
2022-06-11 22:52:15 -0700

    parsing response into an array

--- PRISM/stanford/supply/sail/generate_config.py
+++ PRISM/stanford/supply/sail/generate_config.py
@@ -4,11 +4,28 @@ from typing import Any, Generic, Optional, Dict, List
 from collections import OrderedDict
 
 
-def parse_sinfo_gpu_info(partition: str = None) -> Dict[Any, Any]:
-    query = subprocess.run(['sinfo  -o "%P %G %N" --partition="{}"'.format(partition)],
+def parse_parition_gpu_info(partition: str = None) -> Dict[Any, Any]:
+    # Run initial subprocess to query for partition information
+    query = subprocess.run(
+    ['sinfo  -o "%P %G %N" --partition="{}"'.format(partition)],
     shell=True,
     stdin=subprocess.PIPE,
     stdout=subprocess.PIPE)
+
+    # Split query response by line
     query_parsed = query.stdout.split(b'\n')
-    print('query_parsed: ', query_parsed)
-parse_sinfo_gpu_info('iris')
+
+    # Split per-line query response by column/space into array
+    # keys line 0
+    query_keys = query_parsed[0].split(b' ')
+    # nested list for subsequent rows
+    query_response = [
+    query_parsed[i].split(b' ') for i in range(len(query_parsed))
+    ]
+    print('query_response: ', query_response)
+
+
+
+
+
+parse_parition_gpu_info('iris')
