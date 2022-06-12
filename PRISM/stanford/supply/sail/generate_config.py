737a191fe90cfdfc9255943fa9193ee87a37a290
ELi Ben-Solomon <jq@Jareds-MacBook-Pro-4.local>
2022-06-11 23:00:30 -0700

    beginning to add parse gres functionality

--- PRISM/stanford/supply/sail/generate_config.py
+++ PRISM/stanford/supply/sail/generate_config.py
@@ -22,8 +22,22 @@ def parse_parition_gpu_info(partition: str = None) -> Dict[Any, Any]:
     query_response = [
     query_parsed[i].split(b' ') for i in range(len(query_parsed))
     ]
-    print('query_response: ', query_response)
+    print('query_response: ', query_response
 
+    # Per line in query_response, accumulate info into dict for config
+    # cleaning query response to remove keys and end token
+    iterable_query = query_resonse[1:-1]
+    for line in iterable_query:
+        #TODO(jq): get rid of hardcoded keys
+        partition_name = line[0]
+        gres = line[1]
+        nodelist = line[2]
+
+        # parse_gres
+        parse_gres(gres)
+
+def parse_gres(gres: byte_string = None) -> List[byte_string]:
+    print(gres)
 
 
 
