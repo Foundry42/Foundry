05074b3919f076241827a69e525518f2108c3d0f
Jared Davis <jaredq@sc.stanford.edu>
2022-07-06 08:02:55 -0700

    tweaks

--- PRISM/stanford/availability/sail/parse_historical_usage_data.py
+++ PRISM/stanford/availability/sail/parse_historical_usage_data.py
@@ -15,26 +15,25 @@ def parse_sacct_response(
     """Run subprocess to parse historical data from slurmdb accounting data.
     """
     query = subprocess.run(
-            ['sacct --starttime{} --allusers --format={}'.format(start_time, DEFAULT_FORMAT)],
+            ['sacct --starttime {} --allusers --format={}'.format(start_time, DEFAULT_FORMAT)],
             shell=True,
             stdin=subprocess.PIPE,
             stdout=subprocess.PIPE
             )
     # split query response by line
     query_parsed = query.stdout.split(b'\n')
-
     # split per-line query response by column/space and convert into an array
     ##  keys lines is 0
-    query_keys = query_parsed[0].split(b' ')
+    query_keys = query_parsed[0].split()
     ## nested list for subsequent rows
     query_response = [
-            query_parsed[i].split(b' ') for i in range(len(query_parsed))
+            query_parsed[i].split() for i in range(len(query_parsed))
             ]
-    print('query_response: ', query_response)
+    print('query_response: ', query_response[0:3])
 
     # Filter query response info into a dictionary for easy probing
     sacct_info = {}
     return True
 
-parse_sacct_reponse(start_time="2022-07-04")
+parse_sacct_response(start_time="2022-07-04")
 
